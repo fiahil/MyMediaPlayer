84b2b153ea298bb87f9720532b58377a1de6bd80
lemonn_v <vlemonnier.epitech@gmail.com>
2013-03-13 17:14:29 +0100

    WMP:Add MediaPlaylistElement.cs

---
+++ MyWmp/Items/MediaPlaylistElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace MyWmp.Items
+{
+    class MediaPlaylistElement : MediaElement
+    {
+        public Playlist Playlist;
+
+        public new void Play()
+        {
+            if (Playlist != null)
+            {
+                Source = new Uri(Playlist.Current.Src);
+            }
+            base.Play();
+        }
+    }
+}
