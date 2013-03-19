5535523f3d648fdbbf1ba5faab521c124266982d
Fiahil <fiahil@gmail.com>
2013-03-19 19:40:03 +0100

    add mvvm layer

---
+++ MyWmp/ViewModel/ControlViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Expression.Interactivity.Core;
+
+namespace MyWmp.ViewModel
+{
+    class ControlViewModel : INotifyPropertyChanged
+    {
+        public ControlViewModel()
+        {
+            this.Play = new ActionCommand(() =>
+                {
+                    this.Text = "/Resources/Pause.png";
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+                });
+            this.Text = "/Resources/Play.png";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text { get; private set; }
+        public ICommand Play { get; private set; }
+    }
+}
