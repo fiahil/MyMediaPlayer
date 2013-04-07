3e508e5ac70f0b5bac3fbba80e06cc292da57c2d
Fiahil <fiahil@gmail.com>
2013-04-07 19:49:26 +0200

    Add theme picker

---
+++ MyWmp/View/Theme.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace MyWmp.View
+{
+    /// <summary>
+    /// Interaction logic for Theme.xaml
+    /// </summary>
+    public partial class Theme
+    {
+        public Theme()
+        {
+            InitializeComponent();
+        }
+
+        private void Theme_OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            this.PopupTheme.IsOpen = this.Visibility == Visibility.Visible;
+        }
+    }
+}
