d1a43ada459d95558584fe72f5f9eeb0c2f47b29
Romain <romain.fournereau@gmail.com>
2013-04-07 13:52:18 +0200

    Adding the OpenDir Function

--- MyWmp/View/MenuLayout.xaml.cs
+++ MyWmp/View/MenuLayout.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using System.Windows;
-using Microsoft.Win32;
+using System.Windows.Forms;
 using MyWmp.ViewModel;
+using MessageBox = System.Windows.MessageBox;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
 namespace MyWmp.View
 {
@@ -17,19 +20,30 @@ namespace MyWmp.View
 
         private void MenuItem_OnClick(object sender, RoutedEventArgs e)
         {
-
-                var f = new OpenFileDialog
-                    {
-                        CheckFileExists = true,
-                        ReadOnlyChecked = true,
-                        Multiselect = true
-                    };
-                f.ShowDialog();
-                if (f.FileNames.Any())
+            var f = new OpenFileDialog
                 {
-                    if (((MenuViewModel) this.DataContext).OpenCommand.CanExecute(f.FileNames))
-                        ((MenuViewModel) this.DataContext).OpenCommand.Execute(f.FileNames);
-                }
+                    CheckFileExists = true,
+                    ReadOnlyChecked = true,
+                    Multiselect = true
+                };
+            f.ShowDialog();
+            if (f.FileNames.Any())
+            {
+                if (((MenuViewModel)this.DataContext).OpenCommand.CanExecute(f.FileNames))
+                    ((MenuViewModel)this.DataContext).OpenCommand.Execute(f.FileNames);
+            }
+        }
+
+        private void MenuItem_OnOpenDir(object sender, RoutedEventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != DialogResult.OK)
+                return;
+
+            var file = new string[2];
+            file[0] = fbd.SelectedPath;
+            if (((MenuViewModel)this.DataContext).OpenCommand.CanExecute(file))
+                ((MenuViewModel)this.DataContext).OpenCommand.Execute(file);
         }
     }
 }
